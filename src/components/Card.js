1e9d854db10561cd576490fd63690566de00c7ee
unknown <suivez@interia.pl>
2023-07-05 14:09:26 +0200

    added some logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,61 @@
 import './Card.css';
 import logo from "../images/logo.svg";
-import iconDollar from "../images/icon-dollar.svg";
-import iconPerson from "../images/icon-person.svg";
 import { useState, useRef } from "react";
 
 function Card() {
 
     const [errorInput, setErrorInput] = useState("");
     const [isActiveError, setIsActivError] = useState(false);
-    const inputRefBill = useRef(null);
+    const [tipAmount, setTipAmount] = useState("0.00");
+    const [totalAmount, setTotalAmount] = useState("0.00");
+    const [isActiveReset, setIsActiveReset] = useState(false);
+    const inputRefBill = useRef("");
     const inputRefPercent = useRef(null);
     const inputRefPeople = useRef("");
+    const inputCustomPercent = useRef(null);
 
     const submitSplitter = (e) => {
         e.preventDefault();
 
-        if(inputRefPeople.current.value == null) {
+        const countTipAmount = () => {
+            let tipAmount = 0;
+            tipAmount = parseFloat(inputRefBill.current.value, 10) * (parseFloat(e.target.value, 10) / 100) / parseFloat(inputRefPeople.current.value, 10);
+            return tipAmount.toFixed(2);
+        }
+        
+        const countTotalAmount = () => {
+            let totalAmount = 0;
+            totalAmount = (parseFloat(inputRefBill.current.value, 10) / parseFloat(inputRefPeople.current.value, 10)) + parseFloat(countTipAmount(), 10);
+            return totalAmount;
+        }
+        
+        if(inputRefPeople.current.value == "") {
+            console.log("ASDASDASD");
             setErrorInput("Can't be zero");
             setIsActivError(true);
+            setIsActiveReset(false);
+        }
+        else if(inputRefBill.current.value == "") {
+            setTipAmount("0.00");
+            setTotalAmount("0.00");
+            setErrorInput("");
+            setIsActiveReset(false);
+        }
+        else {
+            setErrorInput("");
+            setIsActivError(false);
+            setTipAmount(countTipAmount());
+            setTotalAmount(countTotalAmount());
+            setIsActiveReset(true);
         }
     }
 
-    const inputField = (icon, inputRef, asd) => {
-        const inputStyle = {
-            background: `url(${icon})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "14px 50%"
-        }
-        
-        return (
-            <div>
-                <input placeholder="0" style={{borderColor: isActiveError ? "red" : "", inputStyle}} ref={{inputRef}}></input>
-            </div>
-        );
+    const resetTipAmounts = () => {
+        setTipAmount("0.00");
+        setTotalAmount("0.00");
+        setIsActiveReset(false);
+        inputRefBill.current.value = 0;
+        inputRefPeople.current.value = 0;
     }
 
     const button = (value) => {
@@ -51,7 +74,7 @@ function Card() {
             <div className="card-content">
                 <div className="card-left">
                     <h5>Bill</h5>
-                    {inputField(iconDollar, inputRefBill, false)}
+                    <input className="input-bill" placeholder="0" ref={inputRefBill}></input>
                     <h5>Select Tip %</h5>
                     <div className="button-box">
                         {button(5)}
@@ -65,7 +88,7 @@ function Card() {
                         <h5>Number of People</h5>
                         <p>{errorInput}</p>
                     </div>
-                    {inputField(iconPerson, inputRefPeople, true)}
+                    <input className="input-people" placeholder="0" style={{borderColor: isActiveError ? "hsl(0, 100%, 68%)" : ""}} ref={inputRefPeople}></input>
                 </div>
                 <div className="card-right">
                     <div className="asd">
@@ -75,7 +98,7 @@ function Card() {
                             <p>/ person</p>
                         </div>
                         <div>
-                            <h1>$0.00</h1>
+                            <h1>${tipAmount}</h1>
                         </div>
                     </div>
                     <div className="box-result">
@@ -84,11 +107,11 @@ function Card() {
                             <p>/ person</p>
                         </div>
                         <div>
-                            <h1>$0.00</h1>
+                            <h1>${totalAmount}</h1>
                         </div>
                     </div>
                     </div>
-                    <button className="btn-reset">Reset</button>
+                    <button className={`${isActiveReset ? "btn-reset-enable" : "btn-reset-disable"}`} onClick={resetTipAmounts}>Reset</button>
                 </div>
             </div>
         </div>
